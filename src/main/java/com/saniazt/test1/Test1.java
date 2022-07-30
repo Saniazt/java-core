cadfa66b3778d19bb800c21b6d4eb3a433bfe395
Saniazt <saniazt@gmail.com>
2022-07-30 21:28:08 +0300

    Test 1 created succesfully

--- src/main/java/com/saniazt/test1/Test1.java
+++ src/main/java/com/saniazt/test1/Test1.java
@@ -2,8 +2,8 @@ package com.saniazt.test1;
 
 public class Test1 {
     public static void main(String[] args) {
-            for(int k = 0; k < 10; k++) {
-                System.out.print(k + "");
+            for(int k = 0; k < 10; k+=2) {
+                System.out.print(k + " ");
             }
     }
 }
