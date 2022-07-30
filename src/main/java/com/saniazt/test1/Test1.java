f47f4fe17549d79ebdff850935c5b5604b3a2a11
Saniazt <saniazt@gmail.com>
2022-07-30 21:22:46 +0300

    Test 1 created succesfully

---
+++ src/main/java/com/saniazt/test1/Test1.java
@@ -0,0 +1,9 @@
+package com.saniazt.test1;
+
+public class Test1 {
+    public static void main(String[] args) {
+            for(int k = 0; k < 10; k++) {
+                System.out.print(k + "");
+            }
+    }
+}
