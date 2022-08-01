36a9adc377a872f3b94669d033367b7b70d8957d
Saniazt <saniazt@gmail.com>
2022-08-01 23:36:37 +0300

    Version 2

--- src/main/java/com/saniazt/test/YtubeMY.java
+++ src/main/java/com/saniazt/test/YtubeMY.java
@@ -10,33 +10,37 @@ import java.util.StringJoiner;
 public class YtubeMY
 {
     public static void main(String[] args) {
-        System.out.println("Hello, lets play the game, What is Your name?");
-        Scanner str = new Scanner(System.in);
-        Str zero;
-        zero = String.nextLine();
+
+            System.out.println("Hello, lets play the game, What is Your name?");
+
+
+            Scanner scanner1 = new Scanner(System.in);
+            String name;
+            name = scanner1.nextLine();
+        System.out.println("Nice to meet you, "+name);
+
+
 
         Scanner num = new Scanner(System.in);
-        int first, second;
+        double first, second;
+
 
         do {
-            System.out.println("Enter your first number:");
-            first = num.nextInt();
+            System.out.println("Okey "+name+" Enter your first number:");
+            first = num.nextDouble();
             if (first == 0)
                 System.out.println("Cannot be 0, choose another number");
         }
         while (first == 0);
 
-        System.out.println("Enter your second number:");
-        second = num.nextInt();
+        System.out.println("Perfect, "+name+" Enter your second number:");
+        second = num.nextDouble();
         if (first > second)
             System.out.println("Great!"+first+">"+second);
         else
             System.out.print("bad luck");
-        switch (first){
-            case 0:
-                System.out.println("Zero cannot be accepted");
-                break;
-        }
+
+
 
     }
 }
