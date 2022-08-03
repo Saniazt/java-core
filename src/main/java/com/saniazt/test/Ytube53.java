24d9b1acbad5c874c081177c89223ff17209c68e
Saniazt <saniazt@gmail.com>
2022-08-03 19:36:38 +0300

    Ver1

---
+++ src/main/java/com/saniazt/test/Ytube53.java
@@ -0,0 +1,15 @@
+package com.saniazt.test;
+
+public class Ytube53 {
+    public static void main(String[] args) {
+
+//        String [] days = new String[];
+//        days[0] = "Monday";
+//        days[1] = "Tuesday";
+//        days[2] = "Wednesday";
+//        for (int i=0; i<days.length; i++){
+//            System.out.println(days[i]);
+//        }
+
+    }
+}
