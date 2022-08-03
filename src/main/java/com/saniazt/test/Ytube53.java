945881334c2b315a525b95d9852191f8f928f011
Saniazt <saniazt@gmail.com>
2022-08-03 21:53:32 +0300

    Ver2

--- src/main/java/com/saniazt/test/Ytube53.java
+++ src/main/java/com/saniazt/test/Ytube53.java
@@ -1,14 +1,45 @@
 package com.saniazt.test;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ytube53 {
     public static void main(String[] args) {
 
+        // done
 //        String [] days = new String[];
 //        days[0] = "Monday";
 //        days[1] = "Tuesday";
 //        days[2] = "Wednesday";
 //        for (int i=0; i<days.length; i++){
 //            System.out.println(days[i]);
+//        }
+//------------
+//        int[] n;
+//        int m;
+//        System.out.println("Please enter array massive: ");
+//        Scanner one = new Scanner(System.in);
+//        m = one.nextInt();
+//        n = new int[m];
+//        for (int i = 0; i < m; i++) {
+//            System.out.println("Enter a[" + i + "] = ");
+//            n[i] = one.nextInt();
+//        }
+//        for (int i = 0; i < m; i++) {
+//            System.out.print(n[i] +",");
+//        }
+//
+//-----------
+
+
+
+
+//        System.out.println(Arrays.toString(n));
+
+
+
+//        for (int i : n) {
+//            System.out.println(i);
 //        }
 
     }
