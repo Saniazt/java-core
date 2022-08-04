e3b897ba42f076a2f43a7b9b2f4f50e12a7aef74
Saniazt <saniazt@gmail.com>
2022-08-04 23:49:44 +0300

    if ver2

--- src/main/java/com/saniazt/test/Ytube7.java
+++ src/main/java/com/saniazt/test/Ytube7.java
@@ -7,14 +7,15 @@ public class Ytube7 {
     public static void main(String[] args) {
 
 
-
         Scanner first = new Scanner(System.in);
         System.out.println("Введите любую цифру");
         int a = first.nextInt();
+        int c = a*a;
 
         if (a > 10) {
             System.out.println("Вы ввели число больше 10");}
         else {
             System.out.println("Вы ввели меньше число 10");}
+        System.out.println("А ваше число в квардате будет "+c);
     }
 }
