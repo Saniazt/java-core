31de1fb245045d1ff8f31ab8482947024d33b900
Saniazt <saniazt@gmail.com>
2022-08-05 22:45:31 +0300

    подФайл куда подставляю

---
+++ src/main/java/com/saniazt/proba01/Proba2.java
@@ -0,0 +1,23 @@
+package com.saniazt.proba01;
+
+public class Proba2 {
+    public static void main(String[] args) {
+      int n = new Proba().summm;
+        System.out.println(n);
+
+        int d = new Proba().c;
+        System.out.println(d);
+
+
+       //  Proba.say("George");так выводим если добавить static в Proba
+        new Proba().say("George"); // так без static
+
+
+
+
+
+
+    }
+
+
+}
