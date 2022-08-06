682868ce184dca138bfb93032cfe2c1481eca286
Saniazt <saniazt@gmail.com>
2022-08-06 10:47:45 +0300

    подФайл куда подставляю+ инлайн пробую ver 2.2

--- src/main/java/com/saniazt/proba01/Proba2.java
+++ src/main/java/com/saniazt/proba01/Proba2.java
@@ -11,6 +11,16 @@ public class Proba2 {
 
         System.out.println(new Proba().g);
 
+        Proba sasha = new Proba(); // создаем в нашем ЭТОМ файле обьект класса проба которые находится в Proba
+
+        System.out.println("Вес Саши: " + sasha.moi_ves + "; Рост Саши: "+ sasha.moy_rost);
+
+        System.out.println();
+
+       sasha.vivod("Sashas");
+
+        sasha.scan_main();
+
        //  Proba.say("George");так выводим если добавить static в Proba
         new Proba().say("George"); // так без static
 
