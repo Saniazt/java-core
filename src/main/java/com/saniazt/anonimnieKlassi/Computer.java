00efa5c5eb5ba9e6a7e4fef65749e4e751aba3ba
Saniazt <saniazt@gmail.com>
2022-08-08 21:51:34 +0300

    08.08.2022 21:51

--- src/main/java/com/saniazt/anonimnieKlassi/Computer.java
+++ src/main/java/com/saniazt/anonimnieKlassi/Computer.java
@@ -4,10 +4,10 @@ public class Computer {
     public static void main(String[] args) {
 
         new Computer(){
-            void superComp(){
-                this.i7.start();
-                this.transfer.start();
-            }
+//            void superComp(){
+//                this.i7.start();
+//                this.transfer.start();
+//            }
         };
     }
 }
