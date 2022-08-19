58656a9e0c3dc5e34beec03ce902a2f0631b5919
saniazt <saniazt@gmail.com>
2022-08-19 10:20:55 +0300

    18.08.2022 10:20

---
+++ src/main/java/com/saniazt/itproger/RealProj2/Main.java
@@ -0,0 +1,12 @@
+package com.saniazt.itproger.RealProj2;
+
+public class Main {
+    public static void main(String[] args) {
+    SimpleGUI app = new SimpleGUI();
+    app.setVisible(true);
+
+    }
+    }
+
+
+
