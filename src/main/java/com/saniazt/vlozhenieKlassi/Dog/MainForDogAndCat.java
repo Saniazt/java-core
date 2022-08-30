e59d3336e4e8246423e52d3ab0cc75054f786de3
saniazt <saniazt@gmail.com>
2022-08-30 19:35:16 +0300

    30.08.2022 19:35

---
+++ src/main/java/com/saniazt/vlozhenieKlassi/Dog/MainForDogAndCat.java
@@ -0,0 +1,20 @@
+package com.saniazt.vlozhenieKlassi.Dog;
+
+public class MainForDogAndCat {
+  public static void main(String[] args) {
+    Dog tessa = new Dog("Tessa");
+    tessa.run();
+    tessa.gav();
+    System.out.println(Dog.Stat.counter);
+    System.out.println(Dog.Stat.id);
+    System.out.println("Hello world!!!!");
+    Cat olivia = new Cat("Olivia", 5);
+    olivia.eat();
+    olivia.sravnenie();
+    olivia.setAge(11);
+    olivia.sravnenie();
+    olivia.eat();
+    olivia.meow("Hello");
+    olivia.meow("Bye");
+  }
+}
