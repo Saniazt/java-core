d5b51162d5af6ef8d809747facecebea1cdd89a7
saniazt <saniazt@gmail.com>
2022-09-12 15:48:57 +0300

    12.09.2022 15:48

---
+++ src/main/java/advancedJava/collections/ArrayList1.java
@@ -0,0 +1,23 @@
+package advancedJava.collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayList1 {
+  public static void main(String[] args) {
+    List<Integer> list = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      list.add(i);
+    }
+    //    System.out.println(list);
+    //    System.out.println(list.get(0));
+    //    System.out.println(list.get(99));
+    //    System.out.println(list.size());
+    /*  for (Integer integer : list) {
+      System.out.println(integer);
+    }*/
+
+
+  }
+}
