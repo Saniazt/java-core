f0c1096517ceea724f1b12822b4b1d7a958d8831
saniazt <saniazt@gmail.com>
2022-09-25 23:04:23 +0300

    25.09.2022 23:04

--- src/main/java/com/saniazt/test/codeWars1.java
+++ src/main/java/com/saniazt/test/codeWars1.java
@@ -1,16 +1,20 @@
 package com.saniazt.test;
 
 public class codeWars1 {
-
-  public int countSheeps(Boolean[] arrayOfSheeps) {
-    int z = 0;
-    for (int i = 0; i < arrayOfSheeps.length; i++) {
-      if (arrayOfSheeps[i] = true) {
-        z++;
-      }
+  public static int getCount(String str) {
+    String[] words = str.split("");
+    int h = 0;
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].equals("a")) h = h + 1;
+      if (words[i].equals("e")) h = h + 1;
+      if (words[i].equals("i")) h = h + 1;
+      if (words[i].equals("o")) h = h + 1;
+      if (words[i].equals("u")) h = h + 1;
     }
-    return z;
+    return h;
   }
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    System.out.println(getCount("aious"));
+  }
 }
