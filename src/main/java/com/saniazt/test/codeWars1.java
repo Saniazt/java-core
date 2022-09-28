8004fef9a92ac35d5a1e276b87447abbc760f64e
saniazt <saniazt@gmail.com>
2022-09-28 21:13:19 +0300

    28.09.2022 21:13

--- src/main/java/com/saniazt/test/codeWars1.java
+++ src/main/java/com/saniazt/test/codeWars1.java
@@ -1,20 +1,95 @@
 package com.saniazt.test;
 
+import java.util.*;
+
 public class codeWars1 {
-  public static int getCount(String str) {
-    String[] words = str.split("");
-    int h = 0;
-    for (int i = 0; i < words.length; i++) {
-      if (words[i].equals("a")) h = h + 1;
-      if (words[i].equals("e")) h = h + 1;
-      if (words[i].equals("i")) h = h + 1;
-      if (words[i].equals("o")) h = h + 1;
-      if (words[i].equals("u")) h = h + 1;
+  public static String longest(String s1, String s2) {
+    var set = new TreeSet<String>();
+
+    for (String s : Arrays.asList(s1, s2)) {
+      for (char c : s.toCharArray()) {
+        set.add(String.valueOf(c));
+      }
     }
-    return h;
+
+    return String.join(", ", set);
   }
 
-  public static void main(String[] args) {
-    System.out.println(getCount("aious"));
+  public static int squareSum(int[] n) {
+    if (n.length == 0) return 0;
+    n = Arrays.stream(n).map(a -> a * a).toArray();
+    return Arrays.stream(n).reduce(Integer::sum).getAsInt();
+  }
+
+  public static String numberToString(int num) {
+    return Integer.toString(num);
+  }
+
+  public static int findSmallestInt(int[] args) {
+    args = Arrays.stream(args).sorted().toArray();
+    return args[0];
+  }
+
+  public static String oddOrEven(int[] array) {
+
+    if (array.length == 0) return "even";
+    int sum = Arrays.stream(array).reduce(Integer::sum).getAsInt();
+    if (sum % 2 == 0) return "even";
+    else return "odd";
+  }
+
+  public static int squareDigits(int n) {
+    String s1 = String.valueOf(n);
+    String[] s2 = s1.split("");
+    int[] arr1 = new int[s2.length];
+    for (int i = 0; i < s2.length; i++) {
+      arr1[i] = Integer.parseInt(s2[i]);
+    }
+    arr1 = Arrays.stream(arr1).map(a -> a * a).toArray();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < arr1.length; i++) {
+      sb.append(arr1[i]);
+    }
+    return Integer.parseInt(String.valueOf(sb));
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    longest("gsdgsdg", "jkfyuxi");
+    final int f = 5;
+    Thread thread =
+        new Thread(
+            () -> {
+              for (int i = 0; i < 10; i++) {
+                System.out.println(i);
+              }
+            });
+    thread.start();
+    thread.join();
+
+    Set<String> list = new HashSet<>();
+    list.add("George");
+    list.add("Ann");
+    list.add("Vitalii");
+    list.add("Sasha");
+    list.add("Sasha");
+    List<Integer> list1 = new ArrayList<>();
+    for (int i = 0; i < list.size(); i++) {
+      list1.add(i, i + 1);
+    }
+
+    Thread thread3 = new Thread(() -> System.out.println(list + list1.toString()));
+    thread3.start();
+    thread3.join();
+
+    Map<Integer, String> map1 = new HashMap<>();
+    Iterator<String> it = list.iterator();
+    for (int i = 0; i < list.size(); i++) {
+      map1.put(list1.get(i), it.next());
+    }
+    System.out.println(map1);
+    int[] n = new int[] {6, 2, 65};
+    squareSum(n);
+    System.out.println(findSmallestInt(n));
+    System.out.println(squareDigits(62781));
   }
 }
